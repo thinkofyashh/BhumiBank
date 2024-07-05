9ac56f33d5c038172498c621674f377b8cd15eb4
yashrawat <yashrawat682@gmail.com>
2024-07-05 16:30:08 +0530

    Added cors to the project

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express=require("express")
+const cors = require('cors');
 const app=express()
 const rootRouter=require("./Routes/index")
 app.use("/api/v1",rootRouter)
+app.use(cors());
 
 app.listen(3000)
