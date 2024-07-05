3e8db1ddaca9a94e39b6d0998f57eef44e8415c7
yashrawat <yashrawat682@gmail.com>
2024-07-05 15:49:01 +0530

    User Moongoose Schema

--- backend/DB/indexx.js
+++ backend/DB/indexx.js
@@ -1,5 +1,17 @@
 const mongoose=require("mongoose")
+const { string } = require("zod")
 
 mongoose.connect("mongodb+srv://thinkofyash:18NovMonday@cluster0.xay1cqj.mongodb.net/BhumiBank")
 
+const UserSchema=mongoose.Schema({
+    username:String,
+    firstname:String,
+    lastname:String,
+    password:String
+})
 
+const User=mongoose.model("Users",UserSchema)
+
+module.exports={
+    Users:User
+}
