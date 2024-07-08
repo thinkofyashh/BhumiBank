a78bf69a11cccbd967bde515965ffc2db9efc594
yashrawat <yashrawat682@gmail.com>
2024-07-08 16:52:43 +0530

    To get the balance of the user we created route for it

--- backend/Routes/account.js
+++ backend/Routes/account.js
@@ -1,5 +1,18 @@
-const express=require("express")
+const express=require("express");
+const authMiddleWares = require("../MiddleWares/middleware");
+const { Accounts } = require("../DB/indexx");
 const router=express.Router();
+
+
+router.get("/balance",authMiddleWares,async function(req,res){
+    const userid=req.userid
+    const account=await Accounts.findOne({
+        userid:userid
+    })
+    res.json({
+        balance:account.balance
+    })
+})
 
 
 
