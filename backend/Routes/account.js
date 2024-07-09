d885d93f6d113001d63fc0cdbfe90bd8d66beaf2
yashrawat <yashrawat682@gmail.com>
2024-07-09 15:02:50 +0530

    Finally fixed the session error occuring due to userid typo

--- backend/Routes/account.js
+++ backend/Routes/account.js
@@ -17,7 +17,8 @@ postman
 
 */
 router.get("/balance",authMiddleWares,async function(req,res){
-    const userid=req.userid
+    const userid=req.userId;
+    console.log(userid)
     const account=await Accounts.findOne({
         userid:userid
     })
@@ -35,45 +36,31 @@ const transferBody=zod.object({
 
 */
 router.post("/transfer",authMiddleWares,async function(req,res){
+try{
+    // started session
 
     const session=await mongoose.startSession();
+    session.startTransaction();
 
-     session.startTransaction();
+    // destructuring of an object and extreacting body
+    const {amount,to}=req.body
 
-
-    const body=req.body
-
-    // checking if the user is sending me the right information if he/she want to send the money.
-
-    const okreport=transferBody.safeParse(body)
-    if(!okreport){
-        return res.json({
-            msg:"Invalid Request"
-        })
-    }
-
-// finding the sender account address by their unique user id
-
+    // checking if the sender account exist or not 
     const senderAccount=await Accounts.findOne({
         userId:req.userId
     }).session(session)
-console.log(req.userId)
-// checking if the sender has amount greater than which he/she sending if not the case returning .
 
-    if(!senderAccount || senderAccount.amount<body.amount){
+    // checking if the balance sended by the sender is less than he/she balance 
+    if(!senderAccount || senderAccount.balance<amount){
+        // aborting the session 
         await session.abortTransaction()
        return res.json({
             msg:"Insufficient Balance"
 
         })
     }
-// finding the account whom the transaction has to be made .
-
-    const toAccount=await Accounts.findOne({
-        userid:body.to
-    }).session(session)
-console.log(body.to)
-// checking if the account exist or not .
+    // checking if the receiver account exist or not
+    const toAccount=await Accounts.findOne({userId:to}).session(session)
 
     if(!toAccount){
         await session.abortTransaction()
@@ -82,21 +69,23 @@ console.log(body.to)
         })
     }
 
-// updating the sender balance with new balance 
+    await Accounts.updateOne({userId:req.userId},{"$inc":{balance:-amount}}).session(session)
 
-    await Accounts.updateOne({userId:req.userId},{"$inc":{balance:-body.amount}}).session(session)
-
-// updating the account with updated value.
-
-    await Accounts.updateOne({userId:body.to},{"$inc":{balance:body.amount}}).session(session)
-
-// if all done returning the message .
-await session.commitTransaction()
-
-   return res.json({
-        msg:"Transfer Successful"
-    })
-
+    // updating the account with updated value.
+    
+        await Accounts.updateOne({userId:to},{"$inc":{balance:amount}}).session(session)
+    
+    // if all done returning the message .
+    await session.commitTransaction()
+    
+       return res.json({
+            msg:"Transfer Successful"
+        })
+}catch(err){
+    await session.abortTransaction();
+    console.log(err)
+    return res.json({msg:"Error transferring funds"})
+}
 })
 
 
