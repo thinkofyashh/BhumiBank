1c11d83beea0117cb3cd96edb6602c91ba8c0fd9
yashrawat <yashrawat682@gmail.com>
2024-07-09 16:50:15 +0530

    Adding routing to the react app

--- frontend/BhumiBank-frontend/src/App.jsx
+++ frontend/BhumiBank-frontend/src/App.jsx
@@ -1,7 +1,36 @@
+import{
+  BrowserRouter,
+  Route,
+  Routes
+} from "react-router-dom";
+
+import {Signup} from "./pages/Signup"
+import {Signin} from "./pages/Signin"
+import {Dashboard} from "./pages/Dashboard"
+import {SendMoney} from "./pages/SendMoney"
+
 function App() {
  return (
     <>
-    hi there 
+    
+
+    <BrowserRouter>
+    <Routes>
+      <Route path="/signup" element={<Signup></Signup>}></Route>
+      <Route path="/signin" element={<Signin></Signin>}></Route>
+      <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+      <Route path="/send" element={<SendMoney></SendMoney>}></Route>
+
+
+    </Routes>
+    </BrowserRouter>
+
+    
+
+
+
+
+
     </>
   )
 }
