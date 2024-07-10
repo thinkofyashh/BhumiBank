fe244ab096ffddc65159fbe4a102737f56649086
yashrawat <yashrawat682@gmail.com>
2024-07-10 18:13:35 +0530

    Error in the frontend

--- frontend/BhumiBank-frontend/src/components/InputBox.jsx
+++ frontend/BhumiBank-frontend/src/components/InputBox.jsx
@@ -1,8 +1,8 @@
-export function InputBox({placeholder,label}){
+export function InputBox({placeholder,label,onChange}){
     return(<div className="py-2">
 
 <div className="font-medium text-black text-sm flex justify-start  pb-2 ">{label}</div>
-    <input placeholder={placeholder} className="text-slate-300 rounded-md px-2 py-1 border-slate-400 border-solid border-sky-500 flex justify-center shadow-inner" ></input>
+    <input onChange={onChange} placeholder={placeholder} className="text-slate-300 rounded-md px-2 py-1 border-slate-400 border-solid border-sky-500 flex justify-center shadow-inner" ></input>
 
 
     </div>
--- frontend/BhumiBank-frontend/src/pages/Signup.jsx
+++ frontend/BhumiBank-frontend/src/pages/Signup.jsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
 import { BottomWarning } from "../components/BottomWarning";
 import { Button } from "../components/Button";
 import { Heading } from "../components/Heading";
 import { InputBox } from "../components/InputBox";
 import { SubHeading } from "../components/SubHeading";
+import axios from "axios"
 
 export function Signup(){
+    const [firstName,setFirstName]=useState("")
+    const [lastName,setLastName]=useState("")
+    const [email,setEmail]=useState("")
+    const [password,setPassword]=useState("")
     return(
         <>
 
@@ -13,12 +19,19 @@ export function Signup(){
                 <div className="rounded-lg bg-white h-max text-center p-2 px-4 w-80">
                     <Heading label={"Sign Up"}></Heading>
                     <SubHeading label={"Enter your infromation to create an account"}></SubHeading>
-                    <InputBox label={"Firstname"} placeholder={"Yash"}></InputBox>
-                    <InputBox label={"Lastname"} placeholder={"Rawat"}></InputBox>
-                    <InputBox label={"Email"} placeholder={"yashrawat@gmail.com"}></InputBox>
-                    <InputBox label={"Password"} placeholder={"123456"}></InputBox>
+                    <InputBox onChange={(e)=>{setFirstName(e.target.value)}} label={"Firstname"} placeholder={"Yash"}></InputBox>
+                    <InputBox onChange={(e)=>{setLastName(e.target.value)}} label={"Lastname"} placeholder={"Rawat"}></InputBox>
+                    <InputBox onChange={(e)=>{setEmail(e.target.value)}} label={"Email"} placeholder={"yashrawat@gmail.com"}></InputBox>
+                    <InputBox onChange={(e)=>{setPassword(e.target.value)}} label={"Password"} placeholder={"123456"}></InputBox>
                     <div className="pt-4">
-                        <Button label={"Sign up"}></Button>
+                        <Button label={"Sign up"} onClick={async ()=>{
+                            await axios.post("http://localhost:3000/api/v1/user/signup",{
+                                username:email,
+                                firstname:firstName,
+                                lastName:lastName,
+                                password:password
+                            })
+                        }}></Button>
                     </div>
                     <BottomWarning label={"Already have an account ?"} buttontext={"Log in"} to={"/signin"}></BottomWarning>
 
