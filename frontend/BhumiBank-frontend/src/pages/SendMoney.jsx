d0ca67172c63a807b324be55a4a6bfcf6e298b92
yashrawat <yashrawat682@gmail.com>
2024-07-11 15:09:28 +0530

    Implemented SendMoney button integrated it with axios post request successfully

--- frontend/BhumiBank-frontend/src/pages/SendMoney.jsx
+++ frontend/BhumiBank-frontend/src/pages/SendMoney.jsx
@@ -1,10 +1,13 @@
 import { useSearchParams } from "react-router-dom";
 import { Heading } from "../components/Heading";
+import axios from "axios"
+import { useState } from "react";
 
 export function SendMoney(){
     const [searchparam]=useSearchParams()
     const id=searchparam.get("id")
     const name =searchparam.get("name")
+    const [amount,setAmount]=useState(0)
     return (
         <div className="bg-gray-600 h-screen flex justify-center">
             <div className="h-full flex flex-col justify-center">
@@ -26,7 +29,7 @@ export function SendMoney(){
 
                 </div>
                 <div className="ml-8 flex flex-col justify-center">
-                <input
+                <input onChange={(e)=>{setAmount(e.target.value)}}
                         type="number"
                         className="flex h-10 w-64 rounded-md border border-input bg-background  text-sm "
                         id="amount"
@@ -34,7 +37,17 @@ export function SendMoney(){
                     />
                 </div>
                 <div className="pt-7 pb-4 ">
-                <button type="button" class="w-64 text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Initiate Transfer</button>
+                <button onClick={(e)=>{
+                    axios.post("http://localhost:3000/api/v1/account/transfer",{
+                        to:id,
+                        amount:amount
+                    },{
+                        headers:{
+                            Authorization: `Bearer ${localStorage.getItem("token")}`
+                        }
+
+                    })
+                }} type="button" class=" w-64 text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Initiate Transfer</button>
                 </div>
 
                 </div>
